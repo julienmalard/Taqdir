03363ea9d516a87aeed6cd0ededabc7a90fa24ce
julienmalard <julien.malard@mail.mcgill.ca>
2018-12-20 11:34:03 -0500

    تھوڑا کام نئی تقدیر پر

--- تقدیر۲/آدادوشمار.py
+++ تقدیر۲/آدادوشمار.py
@@ -1,18 +1,30 @@
+import pandas as pd
+
+
 class آدادوشمار(object):
-    def __init__(خود, آداد):
-        خود.آداد = آداد
+    def __init__(خود, سے, تک, آداد=None):
+        ستون = ['بارش', 'شمسی_تابکاری', 'درجہ_حرارت_زیادہ', 'درجہ_حرارت_کم', 'درجہ_حرارت_اوسط']
+        خود.آداد = pd.DataFrame(آداد, columns=ستون, index=pd.period_range(سے, تک))
+        خود.سے = سے
+        خود.تک = تک
 
-    def روزانا(خود):
-        pass
+    def روزانہ(خود):
+        return خود.آداد
 
-    def مہینی(خود):
-        pass
+    def ماہانہ(خود):
+        return خود.آداد.resample('M').mean()
 
-    def سالنی(خود):
-        pass
+    def سالانہ(خود):
+        return خود.آداد.resample('Y').mean()
+
+    def لاپتہ(خود, سارہ=False):
+        if سارہ:
+            return خود.آداد[خود.آداد.isnull().all(axis=1)].index
+        else:
+            return خود.آداد[خود.آداد.isnull().any(axis=1)].index
 
     def لکھنا(خود):
         pass
 
     def __add__(خود, دوسرہ):
-        pass
+        return آدادوشمار(خود.سے, خود.تک, آداد=خود.آداد.combine_first(دوسرہ))
---
+++ تقدیر۲/ذرائع/__init__.py
@@ -0,0 +1,3 @@
+from .جمع import جمع
+from .مرکسم import مرکسم_جال, مرکسم
+from .ناسا import ناسا
