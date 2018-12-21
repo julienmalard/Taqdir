171faebe411857aa781841eb1ffbdca59bec3902
julienmalard <julien.malard@mail.mcgill.ca>
2018-12-21 17:03:34 -0500

    دیسات پر بہت کام

--- تقدیر۲/ذرائع/مرکسم٥_جال.py
+++ تقدیر۲/ذرائع/مرکسم٥_جال.py
@@ -1,5 +1,5 @@
+import calendar
 import os
-import shutil
 import time
 import zipfile
 
@@ -18,15 +18,21 @@ class مرکسم۵_جال(مرکسم۵):
         خود.اختیار_فایئرفاکس = webdriver.FirefoxProfile()
         خود.اختیار_فایئرفاکس.set_preference('browser.download.folderList', 2)
         خود.اختیار_فایئرفاکس.set_preference('browser.download.manager.showWhenStarting', False)
-        خود.اختیار_فایئرفاکس.set_preference('browser.download.dir', خود.راستہ)
         خود.اختیار_فایئرفاکس.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/zip')
 
     def _کوائف_بنانا(خود, سلسلہ_سال, چوڑائی, طول, بلندی, خاکے):
 
         نام = 'TQDR'
-        راستہ_نتیجہ = os.path.join(خود.راستہ, '{}.zip'.format(نام))
+        راستہ_نتیجہ = os.path.join(خود.راستے, '{}.zip'.format(نام))
+
+        خود.اختیار_فایئرفاکس.set_preference('browser.download.dir', خود.راستے.راستے_پانا(چوڑائی, طول, بلندی))
 
         for سال in سلسلہ_سال:
+
+            راستہ_کا_نام = خود._راستہ_نتیجہ(چوڑائی, طول, بلندی, سال=سال, خاکے=خاکے)
+            if خود._rasta_bhari_hai(راستہ_کا_نام):
+                return
+            os.makedirs(راستہ_کا_نام)
 
             فایئرفاکس = webdriver.Firefox(firefox_profile=خود.اختیار_فایئرفاکس)
             try:
@@ -40,22 +46,22 @@ class مرکسم۵_جال(مرکسم۵):
 
             فایئرفاکس.find_element_by_name('place').send_keys(نام)
 
-            if خود.نمونہ == '11111111111111111':
+            if خود.نمونے == '11111111111111111':
                 فایئرفاکس.find_element_by_link_text('Select All Models').click()
             else:
                 ف_نمونہ = [
                     'bcc1', 'bcc2', 'CSIR', 'FIOE', 'GFD1', 'GFD2', 'GFD3', 'GIS1', 'GIS2', 'HadG', 'IPS1', 'IPS2',
                     'MIR2', 'MIR3', 'MIR1', 'MRIC', 'NorE'
                 ]
-                for ن, نام in zip(خود.نمونہ, ف_نمونہ):
+                for ن, نام in zip(خود.نمونے, ف_نمونہ):
                     if int(ن):
                         فایئرفاکس.find_element_by_id('chkbx_' + نام).click()
 
             فایئرفاکس.find_element_by_xpath('//input[@value="{}"]'.format(خاکے)).click()
 
-            فایئرفاکس.find_element_by_name('yearsimulation').send_keys(str(سال))
+            فایئرفاکس.find_element_by_name('yearsimulation').send_keys(str(سال if سال >= 2010 else 2010))
 
-            فایئرفاکس.find_element_by_name('numrep').send_keys(str(10))
+            فایئرفاکس.find_element_by_name('numrep').send_keys(str(12 if calendar.isleap(سال) else 40))
 
             فایئرفاکس.find_element_by_id('BtnRun').click()
 
@@ -84,15 +90,14 @@ class مرکسم۵_جال(مرکسم۵):
             while not os.path.isfile(راستہ_نتیجہ):
                 pass
 
-            راستہ_کا_نام = os.path.join(خود.راستہ, 'TQDR_11111111111111111_{}_{}'.format(خاکے, سال))
-            if os.path.isdir(راستہ_کا_نام):
-                shutil.rmtree(راستہ_کا_نام)
-
-            os.makedirs(راستہ_کا_نام)
-
-            with zipfile.ZipFile(راستہ_نتیجہ) as az:
-                az.extractall(path=راستہ_کا_نام)
+            with zipfile.ZipFile(راستہ_نتیجہ) as زیپ:
+                زیپ.extractall(path=راستہ_کا_نام)
 
             os.remove(راستہ_نتیجہ)
 
             فایئرفاکس.close()
+
+            for م in os.listdir(راستہ_کا_نام):
+                نام = os.path.splitext(م)[0]
+                if calendar.isleap(2000 + int(نام[-4:-2])) != calendar.isleap(سال):
+                    os.remove(م)
