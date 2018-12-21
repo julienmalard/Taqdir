e19da8ebe1fa01013c73aea8f82851e6f75c2e23
julienmalard <julien.malard@mail.mcgill.ca>
2018-12-21 17:03:34 -0500

    دیسات پر بہت کام

--- تقدیر۲/ذرائع/دیسات.py
+++ تقدیر۲/ذرائع/دیسات.py
@@ -1,27 +1,30 @@
-from تقدیر۲.ذریعہ import ذریعہ
-from tradssat import WTHFile
+import datetime
 
-class ناسا(ذریعہ):
-    
+from tradssat import WTHFile
+from تقدیر۲.ذریعہ import ذریعہ
+
+
+class دیسات(ذریعہ):
+
     def __init__(خود, مسل):
         خود.مسل = مسل
+
     def کوائف_پانا(خود, سے, تک, چوڑائی, طول, بلندی, خاکے='۸۔۵ََ'):
-
         اعداد_پاندس = خود._پاندس_بنانا(سے, تک)
-        koaf_dssat = WTHFile(خود.مسل)
+        کوائف_دیسات = WTHFile(خود.مسل)
 
-        lat_dssat = koaf_dssat.get_value('LAT')
-        long_dssat = koaf_dssat.get_value('LONG')
-        elev_dssat = koaf_dssat.get_value('ELEV')
-        if lat_dssat != چوڑائی or long_dssat != طول or (بلندی is not None and بلندی != elev_dssat):
+        چوڑائی_دیسات = کوائف_دیسات.get_value('LAT')
+        طول_دیسات = کوائف_دیسات.get_value('LONG')
+        بلندی_دیسات = کوائف_دیسات.get_value('ELEV')
+        if چوڑائی_دیسات != چوڑائی or طول_دیسات != طول or (بلندی is not None and بلندی != بلندی_دیسات):
             return
-        
-        dssat_se_bharna(koaf_dssat, اعداد_پاندس)
-        
+
+        دیسات_سے_بھرنا(کوائف_دیسات, اعداد_پاندس, سال=None)
 
         return اعداد_پاندس
 
-def دیسات_پھڑھنا(مسل, اعداد_پاندس):
+
+def دیسات_سے_بھرنا(مسل, اعداد_پاندس, سال):
     'SRAD  TMAX  TMIN  RAIN'
 
     ستون = {
@@ -29,14 +32,16 @@ def دیسات_پھڑھنا(مسل, اعداد_پاندس):
         'شمسی_تابکاری': 'SRAD',
         'درجہ_حرارت_زیادہ': 'TMAX',
         'درجہ_حرارت_کم': 'TMIN',
-        'درجہ_حرارت_اوسط': 'TEMP'
     }
 
-    for تاریخ in اعداد_پاندس.index:
-        for س, س_ناسا in ستون.items():
-            اعداد_پاندس.loc[تاریخ][س] =
+    for ش, تاریخ in enumerate(مسل.get_value('DATE')):
+        if سال is None:
+            raise NotImplementedError
+        else:
+            اسلی_تاریخ = datetime.date(سال, 1, 1) + datetime.timedelta(days=int(تاریخ[-3:]) - 1)
 
-    for mt, mt_DSSAT in mts_.items():
-        pass
+        if اسلی_تاریخ in اعداد_پاندس.index:
+            for مت, مت_دیسات in ستون.items():
+                اعداد_پاندس.loc[اسلی_تاریخ][مت] = مسل.get_value(مت_دیسات)[ش]
 
-    درجہ_حرارت_اوسط = نمپی.add(درجہ_حرارت_زیادہ, درجہ_حرارت_کم) / 2
+    # درجہ_حرارت_اوسط = نمپی.add(درجہ_حرارت_زیادہ, درجہ_حرارت_کم) / 2
