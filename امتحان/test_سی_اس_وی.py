0e40951e68630d54e0f7965f2aa9908f6b706faa
julienmalard <julien.malard@mail.mcgill.ca>
2018-12-24 19:35:47 -0500

    اور کام

--- امتحان/test_سی_اس_وی.py
+++
@@ -1,5 +0,0 @@
-import unittest
-
-
-class امتحان_سی_اس_وی(unittest.TestCase):
-    pass
---
+++ تقدیر۲/ذرائع/جسان.py
@@ -0,0 +1,44 @@
+import json
+
+import pandas as pd
+from chardet import UniversalDetector
+from تقدیر۲.ذریعہ import ذریعہ_نکتہ
+
+
+class جسان(ذریعہ_نکتہ):
+
+    def __init__(خود, مسل, چوڑائی, طول, بلندی=None, خاکے=None, تبدل_ستون=None):
+        super().__init__(چوڑائی, طول, بلندی, خاکے, تبدل_ستون)
+
+        if isinstance(مسل, str):
+            ضابطہ = _ضابطہ_بندی(مسل)
+            with open(مسل, 'r', encoding=ضابطہ) as م:
+                خود.جسان = json.load(م)
+        else:
+            خود.جسان = مسل
+
+    def _کوائف_بھرنا(خود, اعداد_پاندس):
+
+        ستون_تاریخ = خود._نام_ستون('تاریخ')
+
+        اعداد_جسان = pd.DataFrame(
+            data={
+                س: خود.جسان[خود._نام_ستون(س)] for س in خود.ستون if خود._نام_ستون(س) in خود.جسان
+            }, index=pd.PeriodIndex(خود.جسان[ستون_تاریخ], freq='D')
+        )
+        اعداد_پاندس.combine_first(اعداد_جسان)
+
+
+def _ضابطہ_بندی(مسل):
+    آلہ = UniversalDetector()
+    with open(مسل, 'rb') as م:
+        for لکیر in م.readlines():
+
+            آلہ.feed(لکیر)
+
+            if آلہ.done:
+                break
+
+    آلہ.close()
+
+    return آلہ.result['encoding']
