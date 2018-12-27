75213c2a2470ce89d250de2ea165b8eca28b9845
julienmalard <julien.malard@mail.mcgill.ca>
2018-12-27 16:02:41 -0500

    اور کام

--- امتحان/test_ناسا.py
+++ امتحان/test_ناسا.py
@@ -1,9 +1,30 @@
 import unittest
-from urllib import request
+
+import pandas as pd
+import pandas.testing as pdt
+import requests
+from تقدیر۲.ذرائع import ناسا
+
+try:
+    جالبینی_رسائی = requests.head("https://power.larc.nasa.gov/cgi-bin/v1/DataAccess.py").status_code == 200
+except requests.exceptions.ConnectionError:
+    جالبینی_رسائی = False
+وجہ = 'ناسا کا جالبین پنہ اب دستیاب نہیں۔'
 
 
 class امتحان_ناسا(unittest.TestCase):
-    pass
+    @staticmethod
+    def _پورا(سے, تک, اعداد):
+        pdt.assert_index_equal(اعداد.dropna(how='all').index, pd.period_range(سے, تک, freq='D'))
 
+    @unittest.skipUnless(جالبینی_رسائی, وجہ)
+    def test_کوائف_پانا(خود):
+        سے, تک = '۲۰۱۷۱۲۱۵', '۲۰۱۸۰۱۱۵'
+        اعداد = ناسا().کوائف_پانا(سے, تک, چوڑائی=11.02, طول=76.96, بلندی=1, خاکے='۴۔۵')
+        خود._پورا(سے, تک, اعداد)
 
-request.urlretrieve("https://power.larc.nasa.gov/cgi-bin/v1/DataAccess.py")
+    @unittest.skipUnless(جالبینی_رسائی, وجہ)
+    def test_بنہ_خاکے(خود):
+        سے, تک = '۲۰۱۷۱۲۱۵', '۲۰۱۸۰۱۱۵'
+        اعداد = ناسا().کوائف_پانا(سے, تک, چوڑائی=11.02, طول=76.96, بلندی=1, خاکے=None)
+        خود._پورا(سے, تک, اعداد)
