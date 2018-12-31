9c76511b69ffb3f684d4c434dfd15d3d7b256138
julienmalard <julien.malard@mail.mcgill.ca>
2018-12-31 11:43:16 -0500

    دستاویزات

--- دستاویزت/source/_static/جاواسکرپت.js
+++ دستاویزت/source/_static/جاواسکرپت.js
@@ -1,7 +1,7 @@
 function myFunction() {
   var h1 = document.documentElement;
   var att = document.createAttribute("dir");
-  att.value = "auto";
+  att.value = "rtl";
   h1.setAttributeNode(att);
 };
 window.onload = function() {
