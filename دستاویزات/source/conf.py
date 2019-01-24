8e90c1d78ccb7f002e6b0d03033f739e304af54e
julienmalard <julien.malard@mail.mcgill.ca>
2019-01-23 20:09:20 -0500

    تسویر_چھوٹی

--- دستاویزات/source/conf.py
+++ دستاویزات/source/conf.py
@@ -112,7 +112,7 @@ def setup(app):
 # 'searchbox.html']``.
 #
 # html_sidebars = {}
-html_logo = "source/_static/تسویر چھوٹی.png"
+html_logo = "source/_static/تسویر_چھوٹی.png"
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
